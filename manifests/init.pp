d2ee7c4e53551e141b43900679712bc7c7e09489
Trey Dockendorf <treydock@tamu.edu>
2012-11-14 11:25:01 -0600

    - Allow rule's name to act as port - Clean up rule class - Add Hiera defined iptables rules after ENC - ENC takes presendence

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,13 @@
 class iptables {
 
-  if $iptables_rules {
-    $rule = string2hash($iptables_rules)
-    create_resources('iptables::rule', $rule)
+  # ENC value takes presedence over Hiera
+  if $::iptables_rules {
+    $iptables_rules = string2hash($::iptables_rules)
+  } elsif hiera('iptables_rules', false) {
+    $iptables_rules = hiera('iptables_rules')
+  } else {
+    $iptables_rules = false
   }
 
+  if $iptables_rules { create_resources('iptables::rule', $iptables_rules) }
 }
--- manifests/rule.pp
+++ manifests/rule.pp
@@ -1,14 +1,22 @@
 define iptables::rule (
-  $port=false,
-  $proto='tcp'
+  $port   = 'UNSET',
+  $proto  = 'tcp'
 ) {
+
+  $port_real = $port ? {
+    'UNSET' => is_integer($name) ? {
+      true    => $name,
+      false   => fail("Must specify port as integer for firewall rule"),
+    },
+    default => $port,
+  }
 
 	firewall { "001 ${name} by Puppet":
 		ensure  => present,
-		action	=> 'accept',
-		dport		=> "${port}",
-		chain		=> 'INPUT',
-		proto		=> "${proto}",
+		action  => 'accept',
+		dport   => $port_real,
+		chain   => 'INPUT',
+		proto   => $proto,
   }
 
 }
