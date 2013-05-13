472cc5398da6d23e48948ce4dbf84bf6351ebddb
Trey Dockendorf <treydock@tamu.edu>
2013-05-13 16:21:23 -0500

    - Workaround

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,8 @@
 class iptables (
 	$force_drop		= 'UNSET'
 ) {
-  include iptables::pre
-  include iptables::post
+#  include iptables::pre
+#  include iptables::post
 
   # Always persist firewall rules
   exec { 'persist-firewall':
@@ -16,8 +16,8 @@ class iptables (
   # first puppet run.
   Firewall {
     notify  => Exec['persist-firewall'],
-    before  => Class['iptables::post'],
-    require => Class['iptables::pre'],
+#    before  => Class['iptables::post'],
+#    require => Class['iptables::pre'],
   }
   Firewallchain {
     notify  => Exec['persist-firewall'],
@@ -27,9 +27,9 @@ class iptables (
   #
   # This will clear any existing rules, and make sure that only rules
   # defined in puppet exist on the machine
-  resources { "firewall":
-    purge => true
-  }
+#  resources { "firewall":
+#    purge => true
+#  }
 
   # Allow parameters from ENC or Hiera to define individual firewall rules
   # ENC value takes presedence over Hiera
